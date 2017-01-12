363137f8e0d81b5fd0d6a36b6aba3e1518ef9038
Jonas  Gerlich <jgerlich@apollon.de>
2017-01-12 11:29:32 +0100

    Refactor package namings

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("design.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 500, 800));
         primaryStage.show();
     }
 
