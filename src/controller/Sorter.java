c9fa7cc4958038834f9c276fac288cfb2dd1564f
Jonas  Gerlich <jgerlich@apollon.de>
2017-01-12 11:48:05 +0100

    Create required classes

---
+++ src/controller/Sorter.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**
+ * Created by jgerlich on 12.01.2017.
+ */
+public class Sorter {
+}
---
+++ src/model/PrimaryWindow.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by jgerlich on 12.01.2017.
+ */
+public class PrimaryWindow {
+}
