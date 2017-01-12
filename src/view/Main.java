86a84fe5307bae88dd354f3d8060c241af967a0d
Jonas  Gerlich <jgerlich@apollon.de>
2017-01-12 17:00:56 +0100

    Set up main window of application

--- src/view/Main.java
+++ src/view/Main.java
@@ -2,17 +2,31 @@ package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("design.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 500, 800));
+        primaryStage.setTitle("Image Sorter");
+        primaryStage.centerOnScreen();
+        primaryStage.getIcons().add(new Image("/img/camera.png"));
+
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setX(primaryScreenBounds.getMinX());
+        primaryStage.setY(primaryScreenBounds.getMinY());
+        primaryStage.setWidth(primaryScreenBounds.getWidth());
+        primaryStage.setHeight(primaryScreenBounds.getHeight());
         primaryStage.show();
     }
 
