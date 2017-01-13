3e1274fbd55af97302d956c29288e44e3700c561
Jonas  Gerlich <jgerlich@apollon.de>
2017-01-13 13:59:37 +0100

    Add attribute and constructor

--- src/controller/Sorter.java
+++ src/controller/Sorter.java
@@ -1,7 +1,21 @@
 package controller;
 
+import java.io.File;
+
 /**
  * Created by jgerlich on 12.01.2017.
  */
 public class Sorter {
+
+    private File imageFile;
+
+    public Sorter(File imageFile) {
+
+        this.imageFile = imageFile;
+
+    }
+
+
+
+
 }
