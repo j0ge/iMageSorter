e1c85adf91a26d6bd766aa19442f28ba455170e9
Jonas  Gerlich <jgerlich@apollon.de>
2017-01-13 13:16:17 +0100

    Set up readMetadata method

--- src/controller/MetadataProcessor.java
+++ src/controller/MetadataProcessor.java
@@ -1,7 +1,34 @@
 package controller;
 
+import java.io.*;
+
 /**
  * Created by jgerlich on 12.01.2017.
  */
 public class MetadataProcessor {
+
+
+
+    public String readMetadata(File imageFile)  {
+
+        // Starting Exiftool, which is used for Reading Metadata
+        try {
+            Process exifTool = new ProcessBuilder("C:/Users/jgerlich/Desktop/exiftool.exe","-s", "-S", "-createdate",
+                    "-filecreatedate", imageFile.getAbsolutePath()).start();
+
+            exifTool.waitFor();
+
+            InputStream inputExiftool = exifTool.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputExiftool));
+            System.out.println(br.readLine());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
 }
