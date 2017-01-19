f861789af556aadc6aee1eda42990daa3b52e938
Jonas  Gerlich <jgerlich@apollon.de>
2017-01-19 12:02:11 +0100

    Set up basic moving-file option

--- src/main/java/controller/FileMover.java
+++ src/main/java/controller/FileMover.java
@@ -1,7 +1,36 @@
 package controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.Files;
+
 /**
  * Created by jgerlich on 12.01.2017.
  */
 public class FileMover {
+
+    File imageFile;
+    File sortedFolder;
+
+    public FileMover (File imageFile, File sortedFolder){
+        this.imageFile = imageFile;
+        this.sortedFolder = sortedFolder;
+    }
+
+
+    public void moveFile ()  {
+        File movedFile = new File(sortedFolder,imageFile.getName());
+
+        try {
+            Files.move(imageFile.toPath(), movedFile.toPath(), StandardCopyOption.ATOMIC_MOVE);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed to move File");
+        }
+
+
+    }
+
+
 }
