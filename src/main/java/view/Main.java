0b91fa74760d9737a2be7ae8fb5354e826baee99
Jonas  Gerlich <Jon9595@web.de>
2017-01-20 14:21:14 +0100

    Create container for image view within design.fxml

--- src/main/java/view/Main.java
+++ src/main/java/view/Main.java
@@ -1,22 +1,17 @@
 package view;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("design.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("design.fxml"));
         primaryStage.setTitle("Image Sorter");
         primaryStage.centerOnScreen();
         primaryStage.getIcons().add(new Image("/img/camera.png"));
